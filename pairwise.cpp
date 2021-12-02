66bd375297c5c19b88071f4cec452e14794659b5
jubuyer <jahmed10@stuy.edu>
2021-12-02 08:48:42 -0500

    task d works

--- pairwise.cpp
+++ pairwise.cpp
@@ -4,5 +4,23 @@
 #include "pairwise.h"
 
 std::vector<int> sumPairWise(const std::vector<int> &v1, const std::vector<int> &v2) {
-  
+  std::vector<int> vect1 (v1);
+  std::vector<int> vect2 (v2);
+  std::vector<int> output;
+
+  if(vect1.size() > vect2.size()) {
+    for(int i = 0; i < vect1.size()-vect2.size(); i++) {
+      vect2.push_back(0);
+    }
+  } else if (vect2.size() > vect1.size()) {
+      for(int j = 0; j < vect2.size()-vect1.size(); j++) {
+        vect1.push_back(0);
+    }
+  }
+
+  for(int k = 0; k < vect1.size(); k++) {
+    output.push_back(vect1[k]+vect2[k]);
+  }
+
+  return output;
 }

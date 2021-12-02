5d4447df20765a1fcd245d687b15ed150acd02b2
jubuyer <jahmed10@stuy.edu>
2021-12-01 20:46:22 -0500

    initial commit

---
+++ vectors.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+#include "vectors.h"
+
+vector<int> makeVector(int n) {
+  std::vector<int> vec;
+
+  for(int i = 0; i < n; i++) {
+    vec.push_back() (i);
+  }
+
+  return vec;
+}
---
+++ vectors.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+
+#pragma once
+
+vector<int> makeVector(int n);

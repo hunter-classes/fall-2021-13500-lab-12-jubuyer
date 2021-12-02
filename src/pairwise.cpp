02e1dad659d164d96082abda6fe569874ff1665f
jubuyer <jahmed10@stuy.edu>
2021-12-02 16:14:02 -0500

    fixed implementation

--- src/pairwise.cpp
+++ src/pairwise.cpp
@@ -9,18 +9,25 @@ std::vector<int> sumPairWise(const std::vector<int> &v1, const std::vector<int> 
   std::vector<int> output;
 
   if(vect1.size() > vect2.size()) {
-    for(int i = 0; i < vect1.size()-vect2.size(); i++) {
-      vect2.push_back(0);
+    for(int i = 0; i < vect2.size(); i++) {
+      output.push_back(vect2[i]+vect1[i]);
     }
-  } else if (vect2.size() > vect1.size()) {
-      for(int j = 0; j < vect2.size()-vect1.size(); j++) {
-        vect1.push_back(0);
+    for(int j = vect2.size(); j < vect1.size(); j++) {
+      output.push_back(vect1[j]);
     }
-  }
+  } else if(vect2.size() > vect1.size()) {
+      for(int k = 0; k < vect1.size(); k++) {
+        output.push_back(vect2[k]+vect1[k]);
+      }
+      for(int l = vect1.size(); l < vect2.size(); l++) {
+        output.push_back(vect2[l]);
+      }
+    }
 
-  for(int k = 0; k < vect1.size(); k++) {
-    output.push_back(vect1[k]+vect2[k]);
-  }
+
+  // for(int k = 0; k < vect1.size(); k++) {
+  //   output.push_back(vect1[k]+vect2[k]);
+  // }
 
   return output;
 }

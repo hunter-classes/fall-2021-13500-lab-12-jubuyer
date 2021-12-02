2b2be0c7719eb53c9917a1a91ae243e216667851
jubuyer <jahmed10@stuy.edu>
2021-12-02 00:05:48 -0500

    minor changes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include "vectors.h"
 #include "optimism.h"
+#include "fusion.h"
 
 int main(int argc, char const *argv[]) {
   //Task A
@@ -19,13 +20,24 @@ int main(int argc, char const *argv[]) {
   }
   //Task B
  	std::cout << "\n\x1B[31mTest for optimism [TASK B]\n\033[0m";
-  std::vector<int> vect3{1,2,-1,3,4,-1,6};
+  std::vector<int> vect3 {1,2,-1,3,4,-1,6};
   std::vector<int> vect4 (goodVibes(vect3)); // returns [1,2,3,4,6]
   for(int k = 0; k < vect4.size(); k++) {
     std::cout << vect4[k] << '\t';
   }
   //Task C
  	std::cout << "\n\x1B[31mTests for fusion [TASK C]\n\033[0m";
+  std::vector<int> v1{1,2,3};
+  std::vector<int> v2{4,5};
+
+  gogeta(v1, v2); // v1 is now [1,2,3,4,5] and v2 is empty.
+
+  for(int l = 0; l < v1.size(); l++) {
+    std::cout << v1[l] << '\t';
+  }
+  for(int m = 0; m < v2.size(); m++) {
+    std::cout << v2[m] << '\t';
+  }
 
   //Task D
   std::cout << "\n\x1B[31mTests for pairwise [TASK D]\n\033[0m";
